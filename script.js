bf1fbf1e17ebbdbb215754b77eb89f34e72b67bd
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-05-23 23:42:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function getFormvalue() {
     //Write your code here
-	const sub= document.querySelector("value='submit'");
-	const Fn= document.querySelector("name='fname'");
-	const Ln= document.querySelector("name='lname'");
+	const sub= document.querySelector('[value="submit"]');
+	const Fn= document.querySelector('[name="fname"]';
+	const Ln= document.querySelector('[name="lname"]');
 
-	sub.addEventlistener("click", pop)
+	sub.addEventListener("click", pop)
 
 	function pop(){
-		alart(Fn.innerText+" "+Ln.innerText);
+		alert(Fn.innerText+" "+Ln.innerText);
 	}
 
 }
